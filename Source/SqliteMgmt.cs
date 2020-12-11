fc30b64e50a6b41366658da630ecf9b456784f6c
Wolfram S <wolframs91@gmail.com>
2020-12-11 14:10:50 +0100

    KILLED SHITTY DATAACCESS.CS

---
+++ Source/SqliteMgmt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.IO;
+
+namespace IHKiller.Source
+{
+    class SqliteMgmt
+    {
+        public SqliteConnection Connection;
+        
+        public SqliteMgmt(string path)
+        {
+            Connection.ConnectionString = path;
+            Connection.Open();
+        }
+    }
+}
